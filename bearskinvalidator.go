ea40ea5b2a2bb307027940edaf6c0a5f8d03db17
derivatan <derivatan@gmail.com>
2021-01-03 21:42:17 +0100

    permissions new structure [WIP]

--- bearskinvalidator.go
+++ bearskinvalidator.go
@@ -3,6 +3,7 @@ package bearskin
 import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"strings"
 )
 
 /*
@@ -19,11 +20,24 @@ func (ue UnauthorizedError) Error() string {
 /*
 Claims are the information that is stored inside a token.
 If the token was verified correctly, these claims represent the truth.
+
+Permissions is a nested map, with arbitrary depths, with strings for keys and a bool as ending value.
+This could be explained by the Backus–Naur form: `Permissions = map[string]Permissions | bool`
+The ending bool represent weather you
 */
 type Claims struct {
 	jwt.StandardClaims
-	UserID string `json:"user-id"`
-	Permissions []string `json:"permissions"`
+	UserID      string      `json:"user-id"`
+	Permissions Permissions `json:"permissions"`
+}
+
+/*
+Permissions should contain either a Permit or the Next permissions.
+The Permit property should only considered if the map is nil.
+*/
+type Permissions struct {
+	Next   map[string]Permissions `json:"n,omitempty"`
+	Permit bool `json:"p,omitempty"`
 }
 
 /*
@@ -47,4 +61,69 @@ func GetClaimsFromVerifiedJwt(publicKey, tokenString string) (*Claims, error) {
 	}
 
 	return token.Claims.(*Claims), nil
+}
+
+/*
+CheckClaimForPermission is just a shortcut to be able to recurse over the other function.
+*/
+func CheckClaimForPermission(claims *Claims, permission string) bool {
+	return checkClaimForPermissionRecursive(claims.Permissions, permission)
+}
+
+func MonkeyFunctionOnlyForTestingPurposes() bool {
+	// how to handle for example different customers... parametric data??????
+
+	// bearskin.users.*
+	// -bearskin.users.delete
+	permissions := Permissions{Next: map[string]Permissions{
+		"bearskin": {Next: map[string]Permissions{
+			"users": {Next: map[string]Permissions{
+				"*": {Permit: true},
+				"delete": {Permit: false},
+			}},
+			"permissions": {},
+			"tokens": {},
+		}},
+		"Other": {},
+		"more other": {},
+	}}
+	permission := "bearskin.users.delete"
+
+	return checkClaimForPermissionRecursive(permissions, permission)
+}
+
+/*
+TODO: fil in this.
+ */
+func checkClaimForPermissionRecursive(permissions Permissions, permission string) bool {
+	var foundStarPermit bool
+	permissionParts := strings.SplitN(permission, ".", 2)
+	if len(permissionParts) > 0 {
+		if permissionParts[0] == "*" {
+			foundStarPermit = true
+		}
+		val, ok := permissions.Next[permissionParts[0]]
+		if !ok {
+			return foundStarPermit
+		}
+		if len(permissionParts) > 1 {
+			return checkClaimForPermissionRecursive(val, permissionParts[1])
+		}
+		return false
+	}
+	return false
+}
+
+/*
+CheckPermission will checks a token if it contains a given permission.
+ */
+func CheckPermission(publicKey, tokenString, permission string) bool {
+	claims, err := GetClaimsFromVerifiedJwt(publicKey, tokenString)
+	if err != nil {
+		return false
+	}
+
+	result := CheckClaimForPermission(claims, permission)
+
+	return result
 }
